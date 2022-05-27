956b72f1bf2a759b1e4a0066df4eafa2b0de4c66
happy741 <llpl1402@naver.com>
2022-05-27 18:01:28 +0900

    project2_220527_7

---
+++ js/main.js
@@ -0,0 +1,139 @@
+
+         //리뷰 이미지 전환
+         $(function(){
+            $('#btn1').click(function(){
+                $('#review_1').fadeIn();
+                $('#review_2').hide();
+                $('#review_3').hide();
+                $('#review_4').hide();
+                $('#review_5').hide();
+            });
+
+            $('#btn2').click(function(){
+                $('#review_2').fadeIn();
+                $('#review_1').hide();
+                $('#review_3').hide();
+                $('#review_4').hide();
+                $('#review_5').hide();
+            });
+
+            $('#btn3').click(function(){
+                $('#review_3').fadeIn();
+                $('#review_1').hide();
+                $('#review_2').hide();
+                $('#review_4').hide();
+                $('#review_5').hide();
+            });
+
+            $('#btn4').click(function(){
+                $('#review_4').fadeIn();
+                $('#review_1').hide();
+                $('#review_3').hide();
+                $('#review_2').hide();
+                $('#review_5').hide();
+            });
+
+            $('#btn5').click(function(){
+                $('#review_5').fadeIn();
+                $('#review_1').hide();
+                $('#review_3').hide();
+                $('#review_4').hide();
+                $('#review_2').hide();
+            });
+        });
+        
+        //리뷰 이미지 전환
+
+
+        //메뉴 이미지 전환
+        $(function(){
+            $('.icon1').click(function(){
+                $('.section_img').css("background-image","url(img/image.jpg)");
+            });
+            $('.icon2').click(function(){
+                $('.section_img').css("background-image","url(img/water2.jpg)");
+            });
+            $('.icon3').click(function(){
+                $('.section_img').css("background-image","url(img/wedding4.jpg)");
+            });
+
+            $('.icon4').click(function(){
+                $('.section_img').css("background-image","url(img/eco2.jpg)");
+            });
+
+            $('.icon5').click(function(){
+                $('.section_img').css("background-image","url(img/food.jpg)");
+            });
+
+
+        });
+        //메뉴 이미지 전환
+
+//라디오 버튼
+    $(function(){
+        $('#btn2').click(function(){
+            $('#btn2').css('background-color','#fbc22c');
+            $('#btn1').css('background-color','white');
+            $('#btn3').css('background-color','white');
+            $('#btn4').css('background-color','white');
+            $('#btn5').css('background-color','white');
+        });
+    });
+    $('#btn3').click(function(){
+            $('#btn3').css('background-color','#fbc22c');
+            $('#btn1').css('background-color','white');
+            $('#btn2').css('background-color','white');
+            $('#btn4').css('background-color','white');
+            $('#btn5').css('background-color','white');
+        });
+        $('#btn1').click(function(){
+            $('#btn1').css('background-color','#fbc22c');
+            $('#btn3').css('background-color','white');
+            $('#btn2').css('background-color','white');
+            $('#btn4').css('background-color','white');
+            $('#btn5').css('background-color','white');
+        });
+        $('#btn4').click(function(){
+            $('#btn4').css('background-color','#fbc22c');
+            $('#btn1').css('background-color','white');
+            $('#btn2').css('background-color','white');
+            $('#btn3').css('background-color','white');
+            $('#btn5').css('background-color','white');
+        });
+        $('#btn5').click(function(){
+            $('#btn5').css('background-color','#fbc22c');
+            $('#btn1').css('background-color','white');
+            $('#btn2').css('background-color','white');
+            $('#btn4').css('background-color','white');
+            $('#btn3').css('background-color','white');
+        });
+ //라디오 버튼
+
+
+      
+ //네비게이션 슬라이드
+        window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    document.getElementById("scroll_nav").style.top = "0";
+  } else {
+    document.getElementById("scroll_nav").style.top = "-70px";
+  }
+}
+//네비게이션 슬라이드
+
+
+function openNav() {
+      document.getElementById("myNav").style.width = "100%";
+    }
+
+    function closeNav() {
+      document.getElementById("myNav").style.width = "0%";
+    }
+
+$(window).resize(function(){
+    if(window.innerWidth<=768){
+        $('#b').show();
+    }
+});
