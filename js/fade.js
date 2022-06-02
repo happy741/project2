64633d1f1bb28c2cccf3bd70e0094c65dd97b93b
happy741 <llpl1402@naver.com>
2022-06-02 16:55:27 +0900

    project2_220602

--- js/fade.js
+++ js/fade.js
@@ -35,4 +35,59 @@ $(function(){
         $('#prev2').hide();
         $('#next2').hide();
     });
+
+    //process
+    $(".s1").click(function(){
+        $(".t0").fadeIn();
+        $(".t1").hide();
+        $(".t2").hide();
+        $(".t3").hide();
+        $(".t4").hide();
+        $(".t5").hide();
+    });
+
+    $(".s2").click(function(){
+        $(".t1").fadeIn();
+        $(".t0").hide();
+        $(".t2").hide();
+        $(".t3").hide();
+        $(".t4").hide();
+        $(".t5").hide();
+    });
+
+    $(".s3").click(function(){
+        $(".t2").fadeIn();
+        $(".t1").hide();
+        $(".t0").hide();
+        $(".t3").hide();
+        $(".t4").hide();
+        $(".t5").hide();
+    });
+
+    $(".s4").click(function(){
+        $(".t3").fadeIn();
+        $(".t1").hide();
+        $(".t2").hide();
+        $(".t0").hide();
+        $(".t4").hide();
+        $(".t5").hide();
+    });
+
+    $(".s5").click(function(){
+        $(".t4").fadeIn();
+        $(".t1").hide();
+        $(".t2").hide();
+        $(".t0").hide();
+        $(".t3").hide();
+        $(".t5").hide();
+    });
+
+    $(".s6").click(function(){
+        $(".t5").fadeIn();
+        $(".t1").hide();
+        $(".t2").hide();
+        $(".t0").hide();
+        $(".t4").hide();
+        $(".t3").hide();
+    });
 });
